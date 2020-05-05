71210e21404026aff09806daee82476ce6592fc4
Kasper Rutten <kasper.rutten@student.ucll.be>
2020-05-05 17:34:55 +0200

    "get / test" in comments gezet

--- apps/pho_web/test/pho_web/controllers/page_controller_test.exs
+++ apps/pho_web/test/pho_web/controllers/page_controller_test.exs
@@ -1,8 +1,10 @@
 defmodule PhoWeb.PageControllerTest do
   use PhoWeb.ConnCase
 
+  """
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
+  """
 end
